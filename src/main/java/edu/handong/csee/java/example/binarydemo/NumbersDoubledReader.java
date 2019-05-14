eaeec7c961bc3546695d81fd31d2eaaf862fbc31
MinYeSeul <21800251@handong.edu>
2019-05-14 21:09:42 +0900

    NumbersDoubledReader

---
+++ src/main/java/edu/handong/csee/java/example/binarydemo/NumbersDoubledReader.java
@@ -0,0 +1,40 @@
+package edu.handong.csee.java.example.binarydemo;
+
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
+public class NumbersDoubledReader {
+
+public static void main(String[] args) {
+		NumbersDoubledReader reader = new NumbersDoubledReader();
+		reader.run();
+	}
+
+	private void run() {
+		String fileName = "numbers.dat";
+		
+		try {
+			ObjectInputStream inputStream = new ObjectInputStream(
+								new FileInputStream(fileName));
+			System.out.println("Reading the nonnegative integers");
+			System.out.println("in the file " + fileName);
+			while(true) {
+				int anInteger = inputStream.readInt();
+				System.out.println(anInteger);
+			}
+		} catch(FileNotFoundException e) {
+			System.out.println("Problem opening the file " + fileName);
+		} catch (EOFException e) {
+			System.out.println("Reached end of the file.");
+		} catch (IOException e) {
+			System.out.println("Problem reading the file " + fileName);
+		}
+	}
+}
+
